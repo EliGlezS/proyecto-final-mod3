0251f7e626b48658aeaa51801829e1dbf73770b5
EliGlez <jelizabethglezs@gmail.com>
2025-02-15 23:22:51 +0000

    Algoritmos revisados, login revisado (falta posible cambio en el background de login), falta revisar producto

--- ProyMod3/proyMod3/src/js/login.js
+++ ProyMod3/proyMod3/src/js/login.js
@@ -7,54 +7,54 @@ const AUTH_URL = "https://fakestoreapi.com/auth/login";
 async function login(username, password) {
     try {
         const response = await fetch(AUTH_URL, {
-            method:"POST",
+            method:"POST", //Método de la solicitud HTTP
             headers: {
                 "Content-Type" : "application/json",
             },
             body: JSON.stringify({username, password}),
         });
 
-        const data = await response.json();
+        const data = await response.json();//repuesta o data en formato JSON
 
-        if (response.ok) {
+        if (response.ok) {//si la respuesta es exitosa, devuelve un token 
             console.log("Login successful", data);
             return data.token;
-        } else {
+        } else {//En caso de error, devuelve un mensaje de que el login a fallado.
             console.log("Login failed", data);
         }
-    } catch (err) {
+    } catch (err) {//Errores en la petición 
         console.error("Login error:", err);
     }
     
 }
 
-//Localizamos el formulario que tenemos en el html 
+//Se localiza el formulario que tenemos en el html 
 const loginForm = document.getElementById("loginForm");
 
- //pendiente
+//Se llama a la función creada para validar los formularios 
 loadValidation();
 
 
-
-//Este tendrá un evento submit
+//A este formulario se le agrega un event listener. 
 loginForm.addEventListener("submit", async (event) => {
     //prevenimos el comportamiento predefinido del navegador 
     event.preventDefault();
 
-    //pendiente
+    //Fuerza que la validación de los campos se ejecuten manualmente.
     forceValidation();
 
-    //Si hay un error en el formulario no envio nada y salgo submit
+    //Si hay algún campo con la clase error en el formulario no se envía.
     if (loginForm.querySelectorAll(".error").length > 0) {
         return;
     }
 
-    //Creamos las constantes nombre, email y token que vendrá dado por la petición
-    const userName = event.target.username.value;
-    const password = event.target.password.value;
-    const token = await login(userName, password);
+    //Se crea las constantes nombre, email y token (que vendrá dado por la petición)
+    const userName = event.target.username.value;//Obtiene el valor de username 
+    const password = event.target.password.value;//Obtiene el valor de password
+    const token = await login(userName, password);//Se llama a la función login para autenticar los datos y que se muestre el token
     console.log(token);
 
+    //Si se obtiene un token, se redirige al usuario a otra página en este caso a la de productos.
     if (token) {
         window.location.href="products.html";
     }
@@ -65,44 +65,55 @@ loginForm.addEventListener("submit", async (event) => {
 
 //------------------------Verificaciones de formulario-----------------------------------
 
+//Validación de los formularios 
 function loadValidation (){
-    
+    //Se busca todos los inputs con la clase .input-alphanumeric
     //Inputs alfanúmericos con caracter _ 
     const inputsAlphanumeric = document.querySelectorAll(".input-alphanumeric");
 
+    //para cada uno de los inputs se añade un evento input, es decir que cada vez que el usuario escribe se ejecuta la validación
     inputsAlphanumeric.forEach((input) =>{
+
         input.addEventListener("input" , (event) => {
 
+            //Se válida que el texto introducido cumpla con la expresión regular (comienza con minúscula, permite minúscula, mayúsculas, números y guiones bajos, además debe tener al menos 3 caracteres)
             const regex = /^(?=[a-z])(?!.*[^a-z0-9_]).{3,}$/;
             const verificationInputAlpNum = regex.test(event.target.value); 
+
+            //Se busca el span cerca del div 
             const span = event.target.closest("div").querySelector("span");
 
+            //Si no cumple con los requisitos de la expresión regular, se agrega una clase error al input (para mostrar los estilos de error), además de mostrar un mensaje de error en el span.
             if(!verificationInputAlpNum){
                 input.classList.add("error");
                 span.style.display = 'block';
                 span.textContent = "Insert an alphanumeric. Text must be start with letter in lower case, and contain _"
                 console.log("Error detectado");
-            }else{
+            }else{//Si es válido se elimina la clase error, se oculta el span y indica que está todo ok.
                 input.classList.remove("error");
                 span.style.display = 'none';
                 span.textContent = "";
-                console.log("Todo ok");
+                console.log("ok");
             }
 
         });
     });
 
     //Inputs alfanúmericos teniendo en cuenta símbolos especiales 
+    //Se realiza lo mismo que en el código anterior salvo que se tiene en cuenta otros inputs con otras validaciones.
 
     const inputsAlphaNumSimb = document.querySelectorAll(".input-alphanumericSimbols");
 
+    //para cada uno de los inputs se añade un evento input, es decir que cada vez que el usuario escribe se ejecuta la validación
     inputsAlphaNumSimb.forEach((input) =>{
         input.addEventListener("input" , (event) => {
 
+            //LA expresión regular indica que debe la contraseña al menos 5 caracteres
             const regex = /^.{5,}$/;
             const verificationInputAlphaNumSimb = regex.test(event.target.value); 
             const span = event.target.closest("div").querySelector("span");
 
+            //Si no es válido pasa lo mismo que en el caso anterior y si es válido también.
             if(!verificationInputAlphaNumSimb){
                 input.classList.add("error");
                 span.style.display = 'block';
@@ -120,22 +131,25 @@ function loadValidation (){
 }
 
 
+//Esta función permite que las validaciones para los campos anteriores se ejecuten manualmente.
 
 function forceValidation() {
     
+    //Selección del input 
     const inputsAlphanumeric = document.querySelectorAll(".input-alphanumeric"); 
     inputsAlphanumeric.forEach(input => {
-        // Crear el evento 'input'
+        // Crear el evento 'input'. Simula la acción de escribir o modificar el contenido de un campo de texto.
         const event = new Event('input', {
-          'bubbles': true,
-          'cancelable': true
+          'bubbles': true, //Puede propagarse hacia arriba en el DOM
+          'cancelable': true //Puede ser cancelado
         });
       
-        // Disparar el evento 'input' en cada elemento
+        // Disparar el evento 'input' en cada campo seleccionado.
         input.dispatchEvent(event);
     });
 
 
+    //Lo mismo que el anterior pero para el otro tipo de input
     const inputsAlphaNumSimb = document.querySelectorAll(".input-alphanumericSimbols");
     inputsAlphaNumSimb.forEach(input => {
         // Crear el evento 'input'
@@ -144,7 +158,7 @@ function forceValidation() {
           'cancelable': true
         });
       
-        // Disparar el evento 'input' en cada elemento
+        // Disparar el evento 'input' en cada campo seleccionado.
         input.dispatchEvent(event);
     });
 }
